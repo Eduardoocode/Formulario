1d49d8f535f4097796ae77fbd930c2987c90aeec
Eduardoocode <eduardo.ocode@gmail.com>
2025-07-01 21:45:17 -0400

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,10 @@
+<form action="receber.php" method="post">     
+  Nome: <input type="text" name="Nome" required><br>  
+  Data de nascimeneto: <input type="date" name="dt_nascimento" required><br>      
+  Genero <select name="genero"><br>      
+    <option value="masculino">Masculino</option>       
+    <option value="feminino">Feminino</option>       
+    <option value="outro">Outro</option>      
+  </select><br>        
+  <input type="submit" value="Cadastrar">    
+</form>
